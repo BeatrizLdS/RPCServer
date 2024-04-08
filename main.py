7804aadfaa4fd9e01f6d8df461bf14ea3d4cb38a
BeatrizLdS <beatriz_l.silva@hotmail.com>
2024-04-08 11:38:41 -0300

    :recycle: refactor: Start both servers

--- main.py
+++ main.py
@@ -4,5 +4,14 @@ import threading
 
 
 if __name__ == '__main__':
-    threading.Thread(target=gameServerRPC.start_game_server, daemon=True).start()
-    # threading.Thread(target=chatServerRPC.start_chat_server, daemon=True).start()
+    # Inicia o servidor de jogos em uma nova thread
+    game_thread = threading.Thread(target=gameServerRPC.start_game_server)
+    game_thread.start()
+
+    # Inicia o servidor de chat em uma nova thread
+    chat_thread = threading.Thread(target=chatServerRPC.start_chat_server)
+    chat_thread.start()
+
+    # Aguarda o término das threads
+    game_thread.join()
+    chat_thread.join()
