e93dcc615f71174fbd34bc73f4e15469a7ade98e
BeatrizLdS <beatriz_l.silva@hotmail.com>
2024-04-10 19:47:24 -0300

    :sparkles: feat: Add .proto files in Server

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     game_thread.start()
 
     # Inicia o servidor de chat em uma nova thread
-    chat_thread = threading.Thread(target=chatServerRPC.start_chat_server)
+    chat_thread = threading.Thread(target=chatServerRPC.start_chat_server) 
     chat_thread.start()
 
     # Aguarda o término das threads
